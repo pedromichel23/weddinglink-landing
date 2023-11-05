6db9f3e9206467e34761a3f076aa9a8f27c41220
Pedro Michel <pedromichel23@gmail.com>
2023-11-04 18:18:17 -0700

    add fetch api, spinner, form validations

---
+++ assets/js/index.js
@@ -0,0 +1,97 @@
+const forms = document.querySelectorAll('.form')
+const btnSubmitHeroForm = document.querySelector('#btn-submit-hero-form')
+const btnSubmitJoinusForm = document.querySelector('#btn-submit-joinus-form')
+const inputNameHeroForm = document.querySelector('#name-hero-form')
+const inputEmailHeroForm = document.querySelector('#email-hero-form')
+const inputPhoneHeroForm = document.querySelector('#phone-hero-form')
+const inputNameJoinUsForm = document.querySelector('#name-joinus-form')
+const inputEmailJoinUsForm = document.querySelector('#email-joinus-form')
+const inputPhoneJoinUsForm = document.querySelector('#phone-joinus-form')
+const textBtnSubmitHeroForm = document.querySelector('.text-btn-hero-form')
+const textBtnSubmitJoinUsForm = document.querySelector('.text-btn-joinus-form')
+const spinnerHeroBtn = document.querySelector('.spinner-hero-btn')
+const spinnerJoinUsBtn = document.querySelector('.spinner-joinus-btn')
+
+const urlTest = 'http://localhost:8080/lead'
+const url = 'https://www.weddinglink.io/lead'
+
+forms.forEach(form => {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+    })
+})
+
+btnSubmitHeroForm.addEventListener('click', () => {
+    const {name, email, phone} = getInputsValues(inputNameHeroForm, inputEmailHeroForm, inputPhoneHeroForm);
+    SendForm(urlTest, name, email, phone, textBtnSubmitHeroForm, spinnerHeroBtn);
+    forms.forEach(form => {
+        if (form.classList.contains('hero-form')) {
+            form.reset();
+        }
+    })
+})
+
+btnSubmitJoinusForm.addEventListener('click', () => {
+    const {name, email, phone} = getInputsValues(inputNameJoinUsForm, inputEmailJoinUsForm, inputPhoneJoinUsForm);
+    SendForm(urlTest, name, email, phone, textBtnSubmitJoinUsForm, spinnerJoinUsBtn);
+    forms.forEach(form => {
+        if (form.classList.contains('joinus-form')) {
+            form.reset();
+        }
+    })
+})
+
+function SendForm(url, name, email, phone, textBtn, spinner) {
+    if (name && email && phone) {
+        textBtn.classList.add('hide')
+        spinner.classList.remove('hide')
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({name, email, phone}),
+            headers: { 'Content-Type': 'application/json'}
+        })
+        .then( res => res.json())
+        .catch(err => console.log(err))
+        .then(response => {
+            if (response.msg) {
+                console.log(response.msg)
+                console.log(response.lead)
+            }
+    
+            if(response.errors) {
+                window.alert(`Error: ${response.errors[0].msg}`)
+            }
+            textBtn.classList.remove('hide');
+            spinner.classList.add('hide');
+        })
+    }
+}
+
+function getInputsValues(inputName, inputEmail, inputPhone) {
+    return {
+        name: inputName.value,
+        email: inputEmail.value,
+        phone: inputPhone.value
+    }
+}
+
+inputPhoneHeroForm.addEventListener('keydown', (event) => {
+    onlyNumbersInput(event, inputPhoneHeroForm)
+})
+
+inputPhoneJoinUsForm.addEventListener('keydown', (event) => {
+    onlyNumbersInput(event, inputPhoneJoinUsForm)
+})
+
+function onlyNumbersInput(event, input) {
+    const character = event.key;
+    if (!/^\d+$/.test(character)) {
+        event.preventDefault()
+    }
+
+    if (event.code == 'Backspace') {
+       const inputText = input.value;
+       input.value = inputText.slice(0, -1);
+    }
+
+}
